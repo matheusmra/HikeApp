8a93073fa691f6735f300f92fab8c8cac2a70c16
Matheus de Almeida <matheustptv@gmail.com>
2024-04-24 16:08:05 -0300

    Initial Commit

---
+++ HikeApp/HikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeAppApp.swift
+//  HikeApp
+//
+//  Created by Matheus Moreira on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
