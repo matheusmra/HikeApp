4f7e0b8b6f9601d09f0017649410d37e4a9b4a7a
matheusmra <matheustptv@gmail.com>
2024-04-26 19:16:00 -0300

    Mudanças no background: animação de bolhas

---
+++ HikeApp/Component/MotionAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  MotionAnimationView.swift
+//  HikeApp
+//
+//  Created by Matheus Moreira on 26/04/24.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MotionAnimationView()
+}
